e842e28a39f7183634f75e62efd87a041bf5d0be
Natalie Liles <natalieliles@gmail.com>
2019-07-02 22:44:59 -0400

    Setup basic route and views.

---
+++ src/components/ParkList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ListItem from './ListItem'
+import ParkDetail from './ParkDetail'
+import { NPS_API, API_KEY } from "./Const"
+
+class ContainerList extends Component {
+
+  render() {
+    return (
+      <div>
+        Park info here
+      </div>
+    )
+  }
+}
+
+export default ParkList
---
+++ src/components/ParkView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ParkDetail extends Component {
+
+  render() {
+    return (
+      <div>Park detail here</div>
+    )
+  }
+
+}
+
+export default ParkDetail
