278d52ad344d518e3ff09dae1276e452ab76ceb4
Natalie Liles <natalieliles@gmail.com>
2019-07-04 13:48:06 -0400

    Style ist page.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const ListItem = ({park}) => {
+  console.log(park)
   return (
-    <Link to={`/parks/${park.parkCode}`}><h3>{park.fullName}</h3></Link>
+     <div className="list-item">
+        <div className="content-div">
+          <span>{park.designation.toUpperCase()}</span>
+          <Link to={`/parks/${park.parkCode}`}><div className="link-title">{park.fullName}</div></Link>
+          <span>Location: {park.states}</span>
+        </div>
+        {park.images.length > 0 &&
+          <div className="image-div">
+            <img className="parkImage" src={park.images[0].url}/>
+          </div>
+        }
+
+    </div>
   )
 }
 
