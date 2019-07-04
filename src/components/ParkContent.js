56de344b9198d77e48299812fb288a6a6121e2fb
Natalie Liles <natalieliles@gmail.com>
2019-07-04 18:18:49 -0400

    Style list item page.

--- src/components/ParkContent.js
+++ src/components/ParkContent.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import Camera from "../images/Camera.png"
+import Location from "../images/location.png"
 
 const ParkContent = ({park}) => {
   const parkImages = park.images && park.images.length > 0
   const parkAddresses = park.addresses && park.addresses.length > 0
-  console.log(parkImages)
+
   return (
-    <div className="container">
-      <div>
-        <h2>{park.fullName}</h2>
-        <p>{park.designation}</p>
-        {parkAddresses &&
-          <p>{park.addresses[0].city}, {park.addresses[0].stateCode}</p>
-        }
+    <div className="content-container">
+      <div className="content-container-title">
+        <h2 className="content-container-header">{park.fullName}</h2>
+        <div>
+          <span>{park.designation && park.designation}</span>
+          {park.designation && <br/>}
+          {parkAddresses &&
+            <div className="location-icon"><img src={Location} alt="location-icon"/><span>{park.addresses[0].city}, {park.addresses[0].stateCode}
+            </span></div>
+          }
+        </div>
+        <p>{park.description}</p>
+        <div className="camera-icon"><img src={Camera} alt="cam-icon"/><p>Photos</p></div>
       </div>
-      <div>{park.description}</div>
-      <div>{
+      <div className="content-container-row">
+      {
         parkImages &&
-        <img className="park-image" src={park.images[0].url}/>
+        park.images.map(image => <div className="content-container-column"><img key={image.id} src={image.url} alt={image.title}/></div>)
       }</div>
     </div>
   );
