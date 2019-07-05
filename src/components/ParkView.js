514b4afca147f188c4b141f3c06939bac1c08a5f
Natalie Liles <natalieliles@gmail.com>
2019-07-05 02:02:50 -0400

    fix scrolling issue.

--- src/components/ParkView.js
+++ src/components/ParkView.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { withRouter } from "react-router";
-import ParkContent from "./ParkContent"
-import { API_KEY, NPS_API } from "./utils/Constants"
+import ParkContent from "./ParkContent";
+import { API_KEY, NPS_API } from "./utils/Constants";
 
 class ParkView extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       park: {},
-    }
+      error: ""
+    };
   }
 
   componentDidMount = () => {
-    this._isMounted = true;
-    this.getPark(this.props.match.params.id)
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
+    this.getPark(this.props.match.params.id);
+  };
 
   getPark(parkCode) {
-    const url = `${NPS_API}/parks?parkCode=${parkCode}&fields=images,addresses&api_key=${API_KEY}`
+    const url = `${NPS_API}/parks?parkCode=${parkCode}&fields=images,addresses&api_key=${API_KEY}`;
     return fetch(url)
-      .then((response) => response.json())
-      .then((park) => {
-        if (this._isMounted === true) {
-          this.setState({ park: park.data[0] })
+      .then(response => response.json())
+      .then(park => {
+        if (park.error) {
+          this.setState({
+            error: "Something went wrong. Please try again later."
+          });
         }
+        this.setState({ park: park.data[0] });
       })
       .catch(error => console.log(error));
   }
 
   render() {
-    const { park } = this.state;
-    return (
-      <ParkContent park={park}/>
-    )
+    return <ParkContent {...this.state} />;
   }
-
 }
 
-export default withRouter(ParkView)
+export default withRouter(ParkView);
